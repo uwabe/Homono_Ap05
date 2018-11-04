6c7e32d805e87559aa784e3b99d63990189d4a72
uwabe <tkmkAnt7>
2018-11-05 00:47:06 +0900

    draft

--- app/src/main/java/com/websarva/wings/android/homono_ap05/MainActivity.java
+++ app/src/main/java/com/websarva/wings/android/homono_ap05/MainActivity.java
@@ -1,23 +1,26 @@
 package com.websarva.wings.android.homono_ap05;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class MainActivity extends FragmentActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
+public class MainActivity extends FragmentActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener, LoaderManager.LoaderCallbacks<ArrayList<HashMap<String, String>>>{
 
     private final MainActivity self = this;
-    //private ProgressBar progressBar;
 
     private Spinner prefSpinner;
     private Spinner lineSpinner;
@@ -50,11 +53,68 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
+        if (i == 0) {
+            // アイテムが選択されていない
+            return;
+        }
+        // アイテムを取得してリクエストにセット
+        HashMap<String, String> item = (HashMap<String, String>) adapterView.getAdapter().getItem(i);
+        Bundle bundle = new Bundle();
+        bundle.putString("name", item.get("name"));
+        // 実行するメソッドを設定
+        int loaderId = -1;
+        switch (adapterView.getId()) {
+            case R.id.PrefSpinner:
+                loaderId = EkidataLoader.PREFECTURES;
+                break;
+            case R.id.LineSpinner:
+                loaderId = EkidataLoader.LINES;
+                break;
+            case R.id.StaSpinner:
+                loaderId = EkidataLoader.STATIONS;
+                break;
+        }
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    @NonNull
+    @Override
+    public Loader<ArrayList<HashMap<String, String>>> onCreateLoader(int i, @Nullable Bundle bundle) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<ArrayList<HashMap<String, String>>> loader,
+                               ArrayList<HashMap<String, String>> list) {
+        if (list == null) {
+            // 何らかの理由により取得できない
+            Toast.makeText(self, "エラー", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // 取得したデータを対象のSpinnerにセットする
+        SimpleAdapter adapter = new SimpleAdapter(
+                self, list, android.R.layout.simple_spinner_item,
+                new String[] {"name"}, new int[] {android.R.id.text1});
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        switch (loader.getId()) {
+            case EkidataLoader.LINES:
+                lineSpinner.setAdapter(adapter);
+                lineSpinner.setOnItemSelectedListener(self);
+                break;
+            case EkidataLoader.STATIONS:
+                staSpinner.setAdapter(adapter);
+                break;
+        }
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<ArrayList<HashMap<String, String>>> loader) {
+        return;
     }
 }
